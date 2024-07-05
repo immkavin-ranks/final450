b2682163c8f20ac39633bb38070c438fd16aab96
immkavin-ranks <immkavin.ofcl@gmail.com>
2024-07-05 20:53:05 +0530

    ReverseArrayExtraArray.java

---
+++ src/final450/array/reverse/ReverseArrayExtraArray.java
@@ -0,0 +1,19 @@
+package final450.array.reverse;
+
+public class ReverseArrayExtraArray {
+    public static void reverseArrayExtraArray(int[] arr) {
+        int[] reversedArr = new int[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            reversedArr[i] = arr[arr.length - i - 1];
+            System.out.print(reversedArr[i] + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] originalArr = { 1, 2, 3, 4, 5 };
+        reverseArrayExtraArray(originalArr);
+    }
+}
+
+//Time O(n)
+//Space O(n)
